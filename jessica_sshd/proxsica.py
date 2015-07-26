92af71078453dbb6df54f39b751eeb638204b589
Marcell Mars <ki.ber@kom.uni.st>
2015-07-26 03:08:22 +0200

    python http proxy with proxy-authorization. modified https://github.com/inaz2/proxy2.

---
+++ jessica_sshd/proxsica.py
@@ -0,0 +1,208 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+import socket
+import ssl
+import select
+import httplib
+import urlparse
+import threading
+import gzip
+import zlib
+import time
+import json
+import re
+import base64
+from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+from SocketServer import ThreadingMixIn
+from cStringIO import StringIO
+from subprocess import Popen, PIPE
+from HTMLParser import HTMLParser
+
+
+class ThreadingHTTPServer(ThreadingMixIn, HTTPServer):
+    address_family = socket.AF_INET6
+    daemon_threads = True
+
+    def handle_error(self, request, client_address):
+        cls, e = sys.exc_info()[:2]
+        if cls is socket.error or cls is ssl.SSLError:
+            pass
+        else:
+            return HTTPServer.handle_error(self, request, client_address)
+
+
+class ProxyRequestHandler(BaseHTTPRequestHandler):
+    timeout = 5
+    lock = threading.Lock()
+
+    def __init__(self, *args, **kwargs):
+        self.tls = threading.local()
+        self.tls.conns = {}
+        self.key = base64.b64encode("username:password")
+
+        BaseHTTPRequestHandler.__init__(self, *args, **kwargs)
+
+    def log_error(self, format, *args):
+        if isinstance(args[0], socket.timeout):
+            return
+        self.log_message(format, *args)
+
+    def do_HEAD(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_AUTHHEAD(self):
+        self.send_response(407)
+        self.send_header('Proxy-Authenticate',
+                         'Basic realm=\"Logan & Jessica\"')
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def check_authorization(self):
+        if self.headers.get('Proxy-Authorization') is None:
+            self.do_AUTHHEAD()
+            self.wfile.write('no auth header received')
+        elif self.headers.get('Proxy-Authorization') == 'Basic '+ self.key:
+            pass
+        elif self.headers.get('Proxy-Authorization') != 'Basic ' + self.key:
+            self.wfile.write('sorry. change your proxy settings...')
+
+    def do_CONNECT(self):
+        self.check_authorization()
+
+        address = self.path.split(':', 1)
+        address[1] = int(address[1]) or 443
+
+        try:
+            s = socket.create_connection(address, timeout=self.timeout)
+        except Exception as e:
+            self.send_error(502)
+            return
+        self.send_response(200, 'Connection Established')
+        self.end_headers()
+
+        conns = [self.connection, s]
+        self.close_connection = 0
+        while not self.close_connection:
+            rlist, wlist, xlist = select.select(conns, [], conns, self.timeout)
+            if xlist or not rlist:
+                break
+            for r in rlist:
+                other = conns[1] if r is conns[0] else conns[0]
+                data = r.recv(8192)
+                if not data:
+                    self.close_connection = 1
+                    break
+                other.sendall(data)
+
+    def do_GET(self):
+        self.check_authorization()
+
+        req = self
+        content_length = int(req.headers.get('Content-Length', 0))
+        req_body = self.rfile.read(content_length) if content_length else None
+
+        if req.path[0] == '/':
+            if isinstance(self.connection, ssl.SSLSocket):
+                req.path = "https://%s%s" % (req.headers['Host'], req.path)
+            else:
+                req.path = "http://%s%s" % (req.headers['Host'], req.path)
+
+        u = urlparse.urlsplit(req.path)
+        scheme, host, path = u.scheme, u.netloc, (u.path + '?' + u.query if u.query else u.path)
+        assert scheme in ('http', 'https')
+        if host:
+            req.headers['Host'] = host
+        req_headers = self.filter_headers(req.headers)
+
+        try:
+            if not host in self.tls.conns:
+                if scheme == 'https':
+                    self.tls.conns[host] = httplib.HTTPSConnection(host, timeout=self.timeout)
+                else:
+                    self.tls.conns[host] = httplib.HTTPConnection(host, timeout=self.timeout)
+            conn = self.tls.conns[host]
+            conn.request(self.command, path, req_body, dict(req_headers))
+            res = conn.getresponse()
+            res_body = res.read()
+        except Exception as e:
+            if host in self.tls.conns:
+                del self.tls.conns[host]
+            self.send_error(502)
+            return
+
+        version_table = {10: 'HTTP/1.0', 11: 'HTTP/1.1'}
+        setattr(res, 'headers', res.msg)
+        setattr(res, 'response_version', version_table[res.version])
+
+        content_encoding = res.headers.get('Content-Encoding', 'identity')
+        res_body_plain = self.decode_content_body(res_body, content_encoding)
+
+        res_headers = self.filter_headers(res.headers)
+
+        self.wfile.write("%s %d %s\r\n" % (self.protocol_version, res.status, res.reason))
+        for line in res_headers.headers:
+            self.wfile.write(line)
+        self.end_headers()
+        self.wfile.write(res_body)
+        self.wfile.flush()
+
+    def filter_headers(self, headers):
+        # http://tools.ietf.org/html/rfc2616#section-13.5.1
+        hop_by_hop = ('connection', 'keep-alive', 'proxy-authenticate', 'proxy-authorization', 'te', 'trailers', 'transfer-encoding', 'upgrade')
+        for k in hop_by_hop:
+            del headers[k]
+        return headers
+
+    def encode_content_body(self, text, encoding):
+        if encoding in ('gzip', 'x-gzip'):
+            io = StringIO()
+            with gzip.GzipFile(fileobj=io, mode='wb') as f:
+                f.write(text)
+            data = io.getvalue()
+        elif encoding == 'deflate':
+            data = zlib.compress(text)
+        elif encoding == 'identity':
+            data = text
+        else:
+            raise Exception("Unknown Content-Encoding: %s" % encoding)
+        return data
+
+    def decode_content_body(self, data, encoding):
+        if encoding in ('gzip', 'x-gzip'):
+            io = StringIO(data)
+            with gzip.GzipFile(fileobj=io) as f:
+                text = f.read()
+        elif encoding == 'deflate':
+            text = zlib.decompress(data)
+        elif encoding == 'identity':
+            text = data
+        else:
+            raise Exception("Unknown Content-Encoding: %s" % encoding)
+        return text
+
+    do_POST = do_GET
+    do_OPTIONS = do_GET
+
+def start_server(HandlerClass=ProxyRequestHandler,
+                 ServerClass=ThreadingHTTPServer,
+                 protocol="HTTP/1.1"):
+    if sys.argv[1:]:
+        port = int(sys.argv[1])
+    else:
+        port = 8080
+    server_address = ('', port)
+
+    HandlerClass.protocol_version = protocol
+    httpd = ServerClass(server_address, HandlerClass)
+
+    sa = httpd.socket.getsockname()
+    print "Serving HTTP Proxy on", sa[0], "port", sa[1], "..."
+    httpd.serve_forever()
+
+
+if __name__ == '__main__':
+    start_server()
