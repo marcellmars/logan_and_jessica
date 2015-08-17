ca2a101cb5fd43d77f743104df815003272a03e0
Marcell Mars <ki.ber@kom.uni.st>
2015-08-17 19:22:21 +0200

    improved badge and popup update...

--- logan_chrome/js/headers.js
+++ logan_chrome/js/headers.js
@@ -1,8 +1,141 @@
+// init variables for ssh tunnel
+
 var userjess = "init";
 var passjess = "init";
 var realmjess = "init";
 var portjess = 8787;
 var ssh_tab_id = -1;
+var ssh_exists = false;
+var proxy_exists = false;
+
+// variables for popup html
+var geo_code = "  ";
+var geo_country = "";
+var status_text = "No tunnel to Jessica. Connected directly...";
+
+var evc = 0;
+var tevc = -1;
+
+var setStatus = function() {
+    ssh_exists = false;
+    proxy_exists = false;
+    chrome.tabs.query({},
+                    function(tabArray) {
+                        tabArray.filter(function(tab,
+                                                index,
+                                                array) {
+                            if (tab.id === ssh_tab_id) {
+                                ssh_exists = true;
+                            }
+                        });
+
+                        chrome.proxy.settings.get({incognito: false},
+                                                    function(details) {
+                                                        if (details.value.mode === "system") {
+                                                            if (ssh_exists === true) {
+                                                                status_text = "Tunnel to Jessica established but not in use. Connected directly...";
+                                                                chrome.browserAction.setBadgeBackgroundColor({color: [253, 128, 68, 128]});
+                                                                chrome.browserAction.setBadgeText({text: geo_code});
+                                                            }
+                                                            else {
+                                                                status_text = "No tunnel to Jessica. Connected directly...";
+                                                                chrome.browserAction.setBadgeText({text: ""});
+                                                            }
+                                                        } else if (details.value.mode === "fixed_servers") {
+                                                            if (ssh_exists === true) {
+                                                                status_text = "Tunnel to Jessica set. Connected via Jessica...";
+                                                                chrome.browserAction.setBadgeBackgroundColor({color: [145, 196, 55, 128]});
+                                                                chrome.browserAction.setBadgeText({text: geo_code});
+                                                            }
+                                                            else {
+                                                                clearProxy();
+                                                                status_text = "No tunnel to Jessica. Connected directly...";
+                                                                chrome.browserAction.setBadgeText({text: ""});
+                                                            }
+                                                        }
+                                                    });
+                    });
+};
+
+var promiseTabs = function(options) {
+    return new Promise(function(resolve, reject) {
+        chrome.tabs.create(options, resolve);
+    });
+};
+
+var compose = function() {
+    evc ++;
+    setStatus();
+    chrome.tabs.query({highlighted:true,
+                       currentWindow:true},
+                      function(tabArray) {
+                          var tab = tabArray[0];
+                          if (tab.url.substr(0, 36) === "https://jessica.memoryoftheworld.org") {
+                              var url = tab.url;
+                              var args = url.split("/");
+                              var userpass = args[args.length -1].split(":");
+                              var port = args[args.length - 2];
+                              var username = userpass[0];
+                              var password = userpass[1];
+                              // if (ssh_tab_active() !== true) {
+                              if (ssh_exists !== true) {
+                                  portjess = port;
+                                  promiseTabs({'url': "../html/nassh.html#tunnel@ssh.pede.rs:443",
+                                               'pinned': true,
+                                               'active': false}).then(function(tab) {
+                                                   ssh_tab_id = tab.id;
+                                               }).then(function() {
+                                                   addProxyAuthorization(username, password);
+                                               }).then(function() {
+                                                   setProxy();
+                                               }).then(function() {
+                                                   setTimeout(setStatus, 500);
+                                               }).then(function() {});
+                              }
+                              else if (username !== userjess) {
+                                  alert("Please, first close previous tunnel.");
+                                  clearProxy();
+                                  closeTab();
+                                  }
+                          }
+                          else if (ssh_exists === true) {
+                              setProxy();
+                              setStatus();
+                          }
+                          else {
+                              chrome.tabs.query({},
+                                                function(tabArray) {
+                                                    var found_tab = false;
+                                                    tabArray.filter(function(tab,
+                                                                             index,
+                                                                             array) {
+                                                        if (tab.url.substr(0, 36) === "https://jessica.memoryoftheworld.org") {
+                                                            alert("Start tunnel while on tab with Jessica's URL");
+                                                            chrome.tabs.update(tab.id, {active: true,
+                                                                                        highlighted: true});
+                                                            found_tab = true;
+                                                        }
+                                                    });
+                                                    if (found_tab === false) {
+                                                        alert("Ask Jessica for a URL to set up a tunnel.");
+                                                    }
+                                                });
+                          };
+                      });
+};
+
+var direct = function() {
+    evc ++;
+    clearProxy();
+    setStatus();
+};
+
+var reset = function() {
+    evc ++;
+    clearProxy();
+    closeTab();
+    setStatus();
+};
 
 var config = {
     mode: "fixed_servers",
@@ -18,12 +151,14 @@ var config = {
 };
 
 var setProxy = function() {
+    evc ++;
     chrome.proxy.settings.set(
         {value: config, scope: 'regular'},
         function() {});
 };
 
 var clearProxy = function() {
+    evc ++;
     chrome.proxy.settings.clear(
         {scope: 'regular'},
         function() {});
@@ -39,31 +174,32 @@ var clearProxy = function() {
 // };
 
 var closeTab = function() {
-chrome.tabs.query({},
-                    function(tabArray) {
-                        tabArray.filter(function(tab,
-                                                index,
-                                                array) {
-                            if (tab.id === ssh_tab_id) {
-                                chrome.tabs.remove(ssh_tab_id);
-                            }
+    evc ++;
+    chrome.tabs.query({},
+                        function(tabArray) {
+                            tabArray.filter(function(tab,
+                                                    index,
+                                                    array) {
+                                if (tab.id === ssh_tab_id) {
+                                    chrome.tabs.remove(ssh_tab_id);
+                                }
+                            });
                         });
-                    });
 };
 
 var authListener = function(details) {
     if (!details.isProxy || details.realm !== realmjess) {
-        console.log("NOT PROXY!");
+        // console.log("NOT PROXY!");
         return {cancel:true};
     } else {
-        console.log(userjess, passjess);
+        // console.log(userjess, passjess);
         return {authCredentials:{username: userjess,
                                  password: passjess}};
     }
 };
 
 var addProxyAuthorization = function (user, pass) {
-    console.log("addProxyAuthorization");
+    // console.log("addProxyAuthorization");
     userjess = user;
     passjess = pass;
     realmjess = "Logan & Jessica " + user.substring(0,8);
@@ -73,3 +209,29 @@ var addProxyAuthorization = function (user, pass) {
                                                  ['blocking']);
 
 };
+
+var tabListener = function() {
+    if (tevc !== evc) {
+        tevc = evc;
+        ssh_exists = false;
+        chrome.tabs.query({},
+                          function(tabArray) {
+                              tabArray.filter(function(tab,
+                                                       index,
+                                                       array) {
+                                  if (tab.id === ssh_tab_id) {
+                                      ssh_exists = true;
+                                  }
+                              });
+                          });
+
+        chrome.browserAction.getBadgeText({},
+                                          function(t) {
+                                              if (t !== "" && ssh_exists === false) {
+                                                  chrome.browserAction.setBadgeText({text: ""});
+                                              }
+                                          });
+    };
+};
+
+chrome.tabs.onUpdated.addListener(tabListener);
--- logan_chrome/js/popup.js
+++ logan_chrome/js/popup.js
@@ -1,24 +1,17 @@
 var bp = chrome.extension.getBackgroundPage();
-var ssh_exists = false;
-var proxy_exists = false;
+var evc = -1;
 
 var setFlag = function(geoip) {
-    var tlag = document.getElementById('tlag');
-    if (tlag !== null) {
-        document.body.removeChild(tlag);
+    // console.log("setFlag! " + evc);
+    if (geoip.country_code !== ""){
+        bp.geo_code = geoip.country_code.toUpperCase();
+        bp.geo_country = geoip.country;
+        bp.setStatus();
     }
-
-    var loc = document.createElement("div");
-    var flg = document.createElement("span");
-    loc.setAttribute('class', 'tlag');
-    loc.setAttribute('id', 'tlag');
-    flg.setAttribute('class', 'flag ' + 'flag-' + geoip.country_code.toLocaleLowerCase());
-
-    var txt = document.createTextNode("Current location: " + geoip.country + "  ");
-    loc.appendChild(txt);
-    loc.appendChild(flg);
-    document.body.appendChild(loc);
-    chrome.browserAction.setBadgeText({text: geoip.country_code.toUpperCase()});
+    else {
+        bp.geo_country = "";
+        bp.geo_code = "  ";
+    }
 };
 
 var setFlags = function() {
@@ -28,115 +21,46 @@ var setFlags = function() {
     document.body.removeChild(script);
 };
 
-var setStatus = function() {
-    ssh_exists = false;
-    proxy_exists = false;
-    chrome.tabs.query({},
-                    function(tabArray) {
-                        tabArray.filter(function(tab,
-                                                index,
-                                                array) {
-                            if (tab.id === bp.ssh_tab_id) {
-                                ssh_exists = true;
-                            }
-                        });
+var setPopUp = function() {
+    document.getElementById("status").innerText = bp.status_text;
 
-                        chrome.proxy.settings.get({incognito: false},
-                                                    function(details) {
-                                                        if (details.value.mode === "system") {
-                                                            if (ssh_exists === true) {
-                                                                document.getElementById("status").innerText = "Tunnel to Jessica established but not in use. Connected directly...";
-                                                                chrome.browserAction.setBadgeBackgroundColor({color: [253, 128, 68, 128]});
-                                                                setFlags();
-                                                            }
-                                                            else {
-                                                                document.getElementById("status").innerText = "No tunnel to Jessica. Connected directly...";
-                                                                chrome.browserAction.setBadgeText({text: ""});
-                                                            }
-                                                        } else if (details.value.mode === "fixed_servers") {
-                                                            if (ssh_exists === true) {
-                                                                document.getElementById("status").innerText = "Tunnel to Jessica set. Connected via Jessica...";
-                                                                chrome.browserAction.setBadgeBackgroundColor({color: [145, 196, 55, 128]});
-                                                                chrome.browserAction.setBadgeText({text: "  "});
-                                                                setFlags();
-                                                            }
-                                                            else {
-                                                                bp.clearProxy();
-                                                                document.getElementById("status").innerText = "No tunnel to Jessica. Connected directly...";
-                                                                chrome.browserAction.setBadgeText({text: ""});
-                                                            }
-                                                        }
-                                                    });
-                    });
+    if (bp.evc !== evc && bp.ssh_exists === true) {
+        setFlags();
+        if (bp.geo_country !== "") {
+            evc = bp.evc;
+        }
+    }
+
+    var tlag = document.getElementById('tlag');
+    if (tlag !== null) {
+        document.body.removeChild(tlag);
+    }
+
+    if (bp.geo_country !== "") {
+        var loc = document.createElement("div");
+        var flg = document.createElement("span");
+        loc.setAttribute('class', 'tlag');
+        loc.setAttribute('id', 'tlag');
+        flg.setAttribute('class', 'flag ' + 'flag-' + bp.geo_code.toLowerCase());
+
+        var txt = document.createTextNode("Current location: " + bp.geo_country + "  ");
+        loc.appendChild(txt);
+        loc.appendChild(flg);
+        document.body.appendChild(loc);
+    }
+    setTimeout(setPopUp, 400);
 };
 
-var promiseTabs = function(options) {
-    return new Promise(function(resolve, reject) {
-        chrome.tabs.create(options, resolve);
-    });
-};
-
-var compose = function() {
-    setStatus();
-    chrome.tabs.query({highlighted:true,
-                       currentWindow:true},
-                      function(tabArray) {
-                          var tab = tabArray[0];
-                          if (tab.url.substr(0, 36) === "https://jessica.memoryoftheworld.org") {
-                              var url = tab.url;
-                              var args = url.split("/");
-                              var userpass = args[args.length -1].split(":");
-                              var port = args[args.length - 2];
-                              var username = userpass[0];
-                              var password = userpass[1];
-                              // if (bp.ssh_tab_active() !== true) {
-                              if (ssh_exists !== true) {
-                                  bp.portjess = port;
-                                  promiseTabs({'url': "../html/nassh.html#tunnel@ssh.pede.rs:443",
-                                               'pinned': true,
-                                               'active': false}).then(function(tab) {
-                                                   bp.ssh_tab_id = tab.id;
-                                               }).then(function() {
-                                                   bp.addProxyAuthorization(username, password);
-                                               }).then(function() {
-                                                   bp.setProxy();
-                                               }).then(function() {
-                                                   setTimeout(setStatus, 2000);
-                                               }).then(function() {});
-                              }
-                              else if (username !== bp.userjess) {
-                                  alert("Please, first close previous tunnel.");
-                                  bp.clearProxy();
-                                  bp.closeTab();
-                                  }
-                          }
-                          else if (ssh_exists === true) {
-                              bp.setProxy();
-                              setStatus();
-                          }
-                      });
-};
-
-var direct = function() {
-    bp.clearProxy();
-    setStatus();
-};
-
-var reset = function() {
-    bp.clearProxy();
-    bp.closeTab();
-    setStatus();
-};
 
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("viajessica").addEventListener("click", compose);
-    setStatus();
+    document.getElementById("viajessica").addEventListener("click", bp.compose);
+    setPopUp();
 });
 
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("direct").addEventListener("click", direct);
+    document.getElementById("direct").addEventListener("click", bp.direct);
 });
 
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("reset").addEventListener("click", reset);
+    document.getElementById("reset").addEventListener("click", bp.reset);
 });
