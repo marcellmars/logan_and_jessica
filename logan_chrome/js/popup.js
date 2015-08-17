a87fbfd027f32448bb0a25a0c380c092bf0e1848
Marcell Mars <ki.ber@kom.uni.st>
2015-08-17 04:55:20 +0200

    added dynamic badge text to the icon..

--- logan_chrome/js/popup.js
+++ logan_chrome/js/popup.js
@@ -2,24 +2,33 @@ var bp = chrome.extension.getBackgroundPage();
 var ssh_exists = false;
 var proxy_exists = false;
 
-var setFlags = function(geoip) {
+var setFlag = function(geoip) {
+    var tlag = document.getElementById('tlag');
+    if (tlag !== null) {
+        document.body.removeChild(tlag);
+    }
+
     var loc = document.createElement("div");
     var flg = document.createElement("span");
     loc.setAttribute('class', 'tlag');
+    loc.setAttribute('id', 'tlag');
     flg.setAttribute('class', 'flag ' + 'flag-' + geoip.country_code.toLocaleLowerCase());
 
     var txt = document.createTextNode("Current location: " + geoip.country + "  ");
     loc.appendChild(txt);
     loc.appendChild(flg);
     document.body.appendChild(loc);
+    chrome.browserAction.setBadgeText({text: geoip.country_code.toUpperCase()});
+};
+
+var setFlags = function() {
+    var script = document.createElement('script');
+    script.src = 'https://www.telize.com/geoip?callback=setFlag';
+    document.body.appendChild(script);
+    document.body.removeChild(script);
 };
 
 var setStatus = function() {
-    var script = document.createElement('script');
-    script.src = 'https://www.telize.com/geoip?callback=setFlags';
-    document.body.appendChild(script);
-    document.body.removeChild(script);
-
     ssh_exists = false;
     proxy_exists = false;
     chrome.tabs.query({},
@@ -37,21 +46,34 @@ var setStatus = function() {
                                                         if (details.value.mode === "system") {
                                                             if (ssh_exists === true) {
                                                                 document.getElementById("status").innerText = "Tunnel to Jessica established but not in use. Connected directly...";
+                                                                chrome.browserAction.setBadgeBackgroundColor({color: [253, 128, 68, 128]});
+                                                                setFlags();
                                                             }
                                                             else {
-                                                                document.getElementById("status").innerText = "No tunnel to Jessica. Connected directrly...";
+                                                                document.getElementById("status").innerText = "No tunnel to Jessica. Connected directly...";
+                                                                chrome.browserAction.setBadgeText({text: ""});
                                                             }
                                                         } else if (details.value.mode === "fixed_servers") {
                                                             if (ssh_exists === true) {
                                                                 document.getElementById("status").innerText = "Tunnel to Jessica set. Connected via Jessica...";
+                                                                chrome.browserAction.setBadgeBackgroundColor({color: [145, 196, 55, 128]});
+                                                                chrome.browserAction.setBadgeText({text: "  "});
+                                                                setFlags();
                                                             }
                                                             else {
                                                                 bp.clearProxy();
                                                                 document.getElementById("status").innerText = "No tunnel to Jessica. Connected directly...";
+                                                                chrome.browserAction.setBadgeText({text: ""});
                                                             }
                                                         }
                                                     });
                     });
+};
+
+var promiseTabs = function(options) {
+    return new Promise(function(resolve, reject) {
+        chrome.tabs.create(options, resolve);
+    });
 };
 
 var compose = function() {
@@ -70,14 +92,17 @@ var compose = function() {
                               // if (bp.ssh_tab_active() !== true) {
                               if (ssh_exists !== true) {
                                   bp.portjess = port;
-                                  bp.addProxyAuthorization(username, password);
-                                  bp.setProxy();
-                                  chrome.tabs.create({'url': "../html/nassh.html#tunnel@ssh.pede.rs:443",
-                                                      'pinned': true},
-                                                     function(tab) {
-                                                         bp.ssh_tab_id = tab.id;
-                                                     });
-                                  setTimeout(2000, setStatus);
+                                  promiseTabs({'url': "../html/nassh.html#tunnel@ssh.pede.rs:443",
+                                               'pinned': true,
+                                               'active': false}).then(function(tab) {
+                                                   bp.ssh_tab_id = tab.id;
+                                               }).then(function() {
+                                                   bp.addProxyAuthorization(username, password);
+                                               }).then(function() {
+                                                   bp.setProxy();
+                                               }).then(function() {
+                                                   setTimeout(setStatus, 2000);
+                                               }).then(function() {});
                               }
                               else if (username !== bp.userjess) {
                                   alert("Please, first close previous tunnel.");
